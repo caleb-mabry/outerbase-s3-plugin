33493add91951d315d05d9ccb0f4c70b68da5e68
Caleb Mabry <caleb.mabry.dev@gmail.com>
2023-09-10 22:57:03 -0400

    Working plugin!

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     mode: 'production',
@@ -7,4 +8,15 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'plugin.bundle.js',
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+              })
+            ]
+      },    
 };
