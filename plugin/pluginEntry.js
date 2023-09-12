d0d97c07ff3eefff14243feab072cd018a4a27b8
Caleb Mabry <caleb.mabry.dev@gmail.com>
2023-09-12 00:51:01 -0400

    Working plugin config

--- plugin/pluginEntry.js
+++ plugin/pluginEntry.js
@@ -1,5 +1,7 @@
 const { OuterbasePluginCell_$PLUGIN_ID } = require("./pluginCell")
+const { OuterbasePluginConfiguration_$PLUGIN_ID } = require("./pluginConfig")
 const { OuterbasePluginEditor_$PLUGIN_ID } = require("./pluginEditor")
 
 window.customElements.define('outerbase-plugin-cell-$PLUGIN_ID', OuterbasePluginCell_$PLUGIN_ID)
 window.customElements.define('outerbase-plugin-editor-$PLUGIN_ID', OuterbasePluginEditor_$PLUGIN_ID)
+window.customElements.define('outerbase-plugin-configuration-$PLUGIN_ID', OuterbasePluginConfiguration_$PLUGIN_ID)
---
+++ plugin/template/templateConfig.js
@@ -0,0 +1,16 @@
+var templateConfiguration = document.createElement('template')
+templateConfiguration.innerHTML = `
+<style>
+    #container {
+        display: flex;
+        height: 100%;
+        overflow-y: scroll;
+        padding: 40px 50px 65px 40px;
+    }
+</style>
+
+<div id="container">
+    
+</div>
+`
+module.exports = { templateConfiguration }
